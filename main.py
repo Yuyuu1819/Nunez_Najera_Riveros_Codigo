f7ff75fbf879fc2e24ae02b6619fa76ea5196e42
Yuyuu1819 <yuliettenunez@gmail.com>
2026-05-25 09:15:53 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+window = Tk()
+window.geometry("450x450")
+window.title("Proyecto Final - Simpson 1/3")
+
+icon = PhotoImage(file='simpson.png')
+window.iconphoto(True,icon)
+window.config(background="pink")
+
+window.mainloop()

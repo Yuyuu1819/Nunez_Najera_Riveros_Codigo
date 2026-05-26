05e5e9d59fbd028397adacc1f1b6fc15e0aad833
Yuyuu1819 <yuliettenunez@gmail.com>
2026-05-26 11:48:56 -0700

    Logic located in the correct corresponding files and working well!

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-from tkinter import *
+from gui_components import SimpsonApp
 
-window = Tk()
-window.geometry("450x450")
-window.title("Proyecto Final - Simpson 1/3")
-
-icon = PhotoImage(file='simpson.png')
-window.iconphoto(True,icon)
-window.config(background="pink")
-
-window.mainloop()
+# Spawn point
+if __name__ == "__main__":
+    app = SimpsonApp()
+    app.mainloop()
